d8f2a4f6ecf6dfe440a308e22d026e270d6eefd0
Franz Sittampalam <shedali@gmail.com>
2023-11-17 03:45:42 +0000

    add xcfile

---
+++ xcfile/flake.nix
@@ -0,0 +1,32 @@
+{
+  # Override nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.xc_src.url = "github:joerdav/xc/main";
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , systems
+    , xc_src
+    }:
+    flake-utils.lib.eachSystem (import systems)
+      (system:
+      let
+        overlays = [
+          (final: prev: { xc = xc_src.defaultPackage.${system}; })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;[
+            fzf
+            xc
+            mdcat
+          ];
+        };
+      });
+}
