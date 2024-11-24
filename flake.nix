e1526bff85fb8b50fc8229a249f91ae142278c98
Franz Sittampalam <shedali@gmail.com>
2024-11-24 18:38:47 +0000

    try different format

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
   description = "Franz flake templates";
 
-  outputs = { self }: {
-    # Expose each template directly
-    templates.bun = { path = ./bun; };
-    templates.xcfile = { path = ./xcfile; };
-    templates.latex = { path = ./latex; };
-    templates.test = { path = ./test-unpack; };
-
-    # Default template
-    defaultTemplate = self.templates.bun;
+  outputs = { ... }: {
+    templates = {
+      bun = {
+        path = ./bun;
+        description = "A template for bun.sh projects";
+      };
+      xcfile = {
+        path = ./xcfile;
+        description = "A template for interactive markdown files";
+      };
+      latex = {
+        path = ./latex;
+        description = "A template for LaTeX projects";
+      };
+      test = {
+        path = ./test-unpack;
+        description = "A template for testing unpacking setups";
+      };
+    };
   };
 }
