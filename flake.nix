e9a55924681ad325cab698cac13f51faa9d7a211
Franz Sittampalam <shedali@gmail.com>
2024-11-24 18:26:40 +0000

    update root nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,14 @@
 {
   description = "Franz flake templates";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
+    # Expose each template directly
+    templates.bun = { path = ./bun; };
+    templates.xcfile = { path = ./xcfile; };
+    templates.latex = { path = ./latex; };
+    templates.test = { path = ./test-unpack; };
 
-    templates = {
-      xcfile = {
-        path = ./xcfile;
-        description="xc file interactive markdown";
-      };
-      latex = {
-        path = ./latex;
-        description="latex editing";
-      };
-      bun = {
-        path = ./bun;
-        description="bun.sh project";
-      };
-      test = {
-        path = ./test-unpack;
-        description="test unpack";
-      };
-    };
-
-    defaultTemplate = self.templates.xcfile;
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    # Default template
+    defaultTemplate = self.templates.bun;
   };
 }
